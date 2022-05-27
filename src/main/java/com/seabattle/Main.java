198ad1223d9c7d01fd5011b1fc25c8f80cd58197
Kiril <55844647+kefady@users.noreply.github.com>
2022-05-27 14:33:45 +0300

    comment example

--- src/main/java/com/seabattle/Main.java
+++ src/main/java/com/seabattle/Main.java
@@ -2,6 +2,12 @@ package com.seabattle;
 
 import com.seabattle.view.Application;
 
+/**
+ * Main class which run programs
+ * @author Kiril Vashchuk
+ * @author Yaroslava Kozhemiako
+ * @author Yaroslava Nehrych
+ */
 public class Main {
     public static void main(String[] args) {
         Application.main(args);
